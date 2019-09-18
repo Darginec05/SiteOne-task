5bb71a1959686d95384d21afd3107e10fd2f277d
Darginec05 <devopsbanda@gmail.com>
2019-09-19 00:56:18 +0200

    Updates

---
+++ precache-manifest.afb3d67abc35ba6f12086587b05b9367.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e0830f11065e53e875545c3690b181",
+    "url": "/SiteOne-task/index.html"
+  },
+  {
+    "revision": "1a96bff1beabe1ff04ad",
+    "url": "/SiteOne-task/static/css/main.eba22878.chunk.css"
+  },
+  {
+    "revision": "d54a186c728c6b94a8cf",
+    "url": "/SiteOne-task/static/js/2.480b0b60.chunk.js"
+  },
+  {
+    "revision": "1a96bff1beabe1ff04ad",
+    "url": "/SiteOne-task/static/js/main.3153e629.chunk.js"
+  },
+  {
+    "revision": "29212461015c4ec9906c",
+    "url": "/SiteOne-task/static/js/runtime~main.da830bef.js"
+  }
+]);
